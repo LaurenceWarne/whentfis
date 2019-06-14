0f27f0e1c2f2043465006c23f899e9f17452afc7
lWarne <laurencewarne@gmail.com>
2019-06-14 13:29:55 +0100

    Initial commit. Init git repo with existing script.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="whentfis",
+    python_requires='>=3',
+    url="",
+    scripts=["bin/whentfis"]
+    version="0.1",
+    author="Laurence Warne",
+    license="MIT",
+    install_requires=[],
+    zip_safe=False
+)
