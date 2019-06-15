9c85a58737b5c80f38ba4960c999c95549524dc9
lWarne <laurencewarne@gmail.com>
2019-06-15 18:09:46 +0100

    Move script to directory whentfis/scripts to use console_scripts feature of setup.py.

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup, find_packages
 
 
+with open("requirements.txt") as f:
+    dependencies = f.readlines()
+
 setup(
     name="whentfis",
+    packages=find_packages(),
+    include_package_data=True,
     python_requires='>=3',
     url="",
-    scripts=["bin/whentfis"]
+    entry_points={
+        'console_scripts':
+        ['whentfis=whentfis.scripts.whentfis:main'],
+    },
     version="0.1",
     author="Laurence Warne",
     license="MIT",
-    install_requires=[],
+    install_requires=dependencies,
     zip_safe=False
 )
